19974484523375b911bfa1f4b099d01290ddae6b
aylinnn27 <aylinguliyeva17224@gmail.com>
2026-04-20 22:43:08 +0400

    Update 5-printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 /**
- * Betty
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("\"Programming is like building a multilingual puzzle");
-	return (0);
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
 }
