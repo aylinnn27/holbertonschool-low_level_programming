ca4bfe6c28f154fb9d6cbe172d3eae0eb15f3b61
aylinnn27 <aylinguliyeva17224@gmail.com>
2026-04-21 00:05:39 +0400

    Update main.h

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,6 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
+#ifndef MAIN_H
+#define MAIN_H
 
+int _putchar(char c);
+void print_alphabet(void);
+
+#endif
 int _putchar(char c);
 void print_alphabet_x10(void);
 int _islower(int c);
